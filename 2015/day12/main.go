ed1dd6150ef4732e6fc80b1aab80f47e4f895651
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-07-23 02:14:25 +0300

    2015 day12 part2

--- 2015/day12/main.go
+++ 2015/day12/main.go
@@ -11,22 +11,28 @@ func main() {
 	rawInput, _ := os.ReadFile("2015/day12/input.txt")
 	input := string(rawInput)
 
-	ans := Part1(input)
+	ans := JsonParser(input, 2)
 	fmt.Printf("ans: %v\n", ans)
 }
 
-func Part1(input string) int {
+func JsonParser(input string, part int) int {
 	var result interface{}
 
 	if err := json.Unmarshal([]byte(input), &result); err != nil {
 		panic(err)
 	}
 
-	val := iterator(result)
-	return val
+	var answer int
+	if part == 1 {
+		answer = Part1Recursion(result)
+	} else if part == 2 {
+		answer = Part2Recursion(result)
+		
+	}
+	return answer
 }
 
-func iterator(val interface{}) int {
+func Part1Recursion(val interface{}) int {
 	ans := 0
 
 	switch i := val.(type) {
@@ -40,13 +46,45 @@ func iterator(val interface{}) int {
 	case map[string]interface{}:
 		sum := 0
 		for _, s := range i {
-			sum += iterator(s)
+			sum += Part1Recursion(s)
 		}
 		return sum
 	case []interface{}:
 		sum := 0
 		for _, j := range i {
-			sum += iterator(j)
+			sum += Part1Recursion(j)
+		}
+		return sum
+	default:
+		fmt.Println("uncaught type", i)
+	}
+	return ans
+}
+
+func Part2Recursion(val interface{}) int {
+	ans := 0
+
+	switch i := val.(type) {
+	case string:
+		if v, err := strconv.Atoi(i); err == nil {
+			return v
+		}
+	case float32:
+	case float64:
+		return int(i)
+	case map[string]interface{}:
+		sum := 0
+		for f, s := range i {
+			if f == "red" || s == "red" {
+				return 0
+			}
+			sum += Part2Recursion(s)
+		}
+		return sum
+	case []interface{}:
+		sum := 0
+		for _, j := range i {
+			sum += Part2Recursion(j)
 		}
 		return sum
 	default:
--- 2015/day12/main_test.go
+++ 2015/day12/main_test.go
@@ -19,8 +19,28 @@ func TestJson(t *testing.T) {
 
 		for i := range tests {
 			t.Run(fmt.Sprintf("test %v", tests[i]), func(t *testing.T) {
-				got := Part1(tests[i])
+				got := JsonParser(tests[i], 1)
 
+				if got != want[i] {
+					t.Errorf("got '%v' want '%v'", got, want[i])
+				}
+			})
+		}
+	})
+
+	t.Run("Part2", func(t *testing.T) {
+		tests := []string{
+			`[1,2,3]`,
+			`[1,{"c":"red","b":2},3]`,
+			`{"d":"red","e":[1,2,3,4],"f":5}`,
+			`[1,"red",5]`,
+		}
+		want := []int{6, 4, 0, 6}
+		
+		for i := range tests {
+			t.Run(fmt.Sprintf("test %v", tests[i]), func(t *testing.T) {
+				got := JsonParser(tests[i], 2)
+				
 				if got != want[i] {
 					t.Errorf("got '%v' want '%v'", got, want[i])
 				}
